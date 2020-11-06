4cba4b6a34b3ca0b9b8b200607b5b805a68509d1
Claudio Olivares <claudio.olivares@zentagroup.com>
2020-11-06 15:31:49 -0300

    version android

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,  ReactiveFormsModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
+    AppRoutingModule,
+    ReactiveFormsModule],
   providers: [
     StatusBar,
     SplashScreen,
@@ -21,4 +26,4 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
@@ -15,13 +16,16 @@ export class HomePage implements OnInit {
     error: null
   };
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private storage: Storage) {
     this.form = new FormGroup({
       user: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required),
     });
   }
   ngOnInit() {
+    if (localStorage.getItem('isLoggedin')) {
+       this.router.navigate(['/login/home']);
+    }
     this.status.data = false;
     this.status.loading = false;
     this.status.error = false;
@@ -36,6 +40,7 @@ export class HomePage implements OnInit {
       if (values.user === 'test@gmail.com' && values.password === '123') {
         this.status.loading = false;
         this.status.data = true;
+        localStorage.setItem('isLoggedin', 'true');
         this.router.navigate(['/login/home']);
       } else {
         this.status.loading = false;
