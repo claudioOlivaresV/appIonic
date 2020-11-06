0cb830dd5b65d0bacec7f413a7269d7c033e55e5
Claudio Olivares <claudio.olivares@zentagroup.com>
2020-11-06 13:59:58 -0300

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  form: FormGroup;
+  status = {
+    data: null,
+    loading: null,
+    error: null
+  };
 
-  constructor() {}
+  constructor(private router: Router) {
+    this.form = new FormGroup({
+      user: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+  ngOnInit() {
+    this.status.data = false;
+    this.status.loading = false;
+    this.status.error = false;
+
+  }
+  login(values) {
+    console.log(values);
+    this.status.data = false;
+    this.status.loading = true;
+    this.status.error = false;
+    setTimeout(() => {
+      if (values.user === 'test@gmail.com' && values.password === '123') {
+        this.status.loading = false;
+        this.status.data = true;
+        this.router.navigate(['/login/home']);
+      } else {
+        this.status.loading = false;
+        this.status.error = true;
+      }
+    }, 3000);
+  }
 
 }
